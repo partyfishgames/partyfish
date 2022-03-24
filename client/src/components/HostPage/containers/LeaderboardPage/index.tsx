fbde80d3d8fa83cdf012ad3ba845f0fc462dac21
Abby Leone <aleone2@nd.edu>
2022-03-23 22:17:10 -0400

    Leaderboard and End Results

--- client/src/components/HostPage/containers/LeaderboardPage/index.tsx
+++ client/src/components/HostPage/containers/LeaderboardPage/index.tsx
@@ -1,11 +1,10 @@
-import { Button, Grid } from "@mui/material";
+import { Button, Grid, Typography, createTheme, ThemeProvider, Paper, Box } from "@mui/material";
 import { useAppDispatch } from "../../../../hooks";
 import gameService from "../../../../services/gameService";
 import socketService from "../../../../services/socketService";
 import { useAppSelector } from "../../../../hooks";
 import { useEffect, useState } from "react";
 import { RiSwordLine, RiHeartFill } from "react-icons/ri";
-import { VscGraph } from "react-icons/vsc";
 import { FaMedal } from "react-icons/fa";
 
 import FadeIn from 'react-fade-in';
@@ -16,6 +15,37 @@ const selectGameStats = (state: { gameStats: any }) => state.gameStats; // selec
 const selectPlayersList = (state: { playerLists: any; }) => state.playerLists; // select for player lists state 
 
 export function LeaderboardPage() {
+
+    const islandTheme = createTheme({
+        typography: {
+            fontFamily: [
+                'Syne Mono', 
+                'monospace'
+            ].join(','),
+            fontSize: 22,
+            fontWeightRegular:500,
+        },
+        palette: {
+            primary: {
+                main: "#A6CF98", // light green
+            },
+            secondary: {
+                main: "#E3CAA5", // light brown
+            }, 
+            success: {
+                main: "#557C55", // dark green
+            },
+            error: {
+                main: "#D29D2B", // burnt yellow
+            },
+            info: {
+                main: "#85F4FF" // cyan blue
+            },
+            background:{
+                paper: "#AD8B73", // dark brown
+            }
+        },
+    });
 
     const dispatch = useAppDispatch(); // included in any component that dispatches actions
 
@@ -102,28 +132,66 @@ export function LeaderboardPage() {
 
     function RoundLeaderboard() {
         return (<div style={{ textAlign: "center" }}>
-            <h1>Leaderboard</h1>
-            <h3>Round Number: {gameStats.roundNumber}</h3>
-            <h3>Correct Answer: {question[parseInt(question[4])]}</h3>
-            <Grid container direction="row" justifyContent="center" alignItems="flex-start">
-                <Grid item md={3}>
-                    <h2>Score<VscGraph /> / Health<RiHeartFill /></h2>
-                    {Object.entries(playerScores).sort((a: any, b: any) => b[1] - a[1]).map((entry, idx) => (
-                        <h4>{idx + 1}. {entry[0]} {entry[1]}</h4>
-                    ))}
-                </Grid>
-                <Grid item md={3}>
-                    <h2>Attacks!</h2>
-                    {attacks.map(attack => <h4>{attack[0]} <RiSwordLine /> {attack[1]}</h4>)}
-                </Grid>
-            </Grid>
-
-            {playerLists.alivePlayers.length > 1 && gameStats.roundNumber < 10 ?
-                <Button onClick={newRound}>Next Round</Button>
-                :
-                <Button onClick={endGame}>See Results!</Button>
-            }
-
+            <ThemeProvider theme={islandTheme}>
+                <Box bgcolor='#D29D2B' sx={{my:3}}>
+                    <Box sx={{mx:3, my:3, py:3}}>
+                        <Paper elevation={3} sx={{py: 2, px: 2}} style={{background: "#557C55"}}>
+                            <Box sx={{mx:14, my:3}}>
+                                <Paper elevation={3} sx={{py: 2, px: 2}}>
+                                    <Typography component='h3' color="secondary">Round {gameStats.roundNumber}</Typography>
+                                </Paper>
+                            </Box>
+                            <Box sx={{mx:10, my:3}}>
+                                <Paper elevation={3} sx={{py: 2, px: 2}} style={{background: '#D29D2B'}}>
+                                    <Typography component='h3'color="#557C55">Leaderboard</Typography>
+                                </Paper>
+                            </Box>
+                            <Box sx={{mx:12, my:3}}>
+                                <Paper elevation={3} sx={{py: 2, px: 2}} >
+                                    <Typography component='h3'color="secondary">Correct Answer: {question[parseInt(question[4])]}</Typography>
+                                </Paper>
+                            </Box>
+                            <Box sx={{mx:10, my:3}}>
+                                <Paper elevation={3} sx={{py: 2, px: 2}} style={{background: "#A6CF98"}}>
+                                    <Grid container direction="row" justifyContent="center" alignItems="flex-start">
+                                        <Grid item md={3} sx={{mx:3}}>
+                                            <Box sx={{mx:1, my:1}}>
+                                                <Paper elevation={3} sx={{py: 1, px: 3}} style={{background: "#E3CAA5"}}>
+                                                    <Typography color="#557C55">Health<RiHeartFill color="#85F4FF" /></Typography>
+                                                </Paper>
+                                            </Box>
+                                            {Object.entries(playerScores).sort((a: any, b: any) => b[1] - a[1]).map((entry, idx) => (
+                                                <Typography color="#557C55">{idx + 1}. {entry[0]} {entry[1]}</Typography>
+                                            ))}
+                                        </Grid>
+                                        <Grid item md={3} sx={{mx:3}}>
+                                            <Box sx={{mx:1, my:1}}>
+                                                <Paper elevation={3} sx={{py: 1, px: 3}} style={{background: "#E3CAA5"}}>
+                                                    <Typography color="#557C55">Attacks<RiSwordLine color="#9B0000"/></Typography>
+                                                </Paper>
+                                            </Box>
+                                            {attacks.map(attack => <Typography color="#557C55">{attack[0]} <RiSwordLine color="#9B0000" /> {attack[1]}</Typography>)}
+                                        </Grid>
+                                    </Grid>
+                                </Paper>
+                            </Box>
+                            {playerLists.alivePlayers.length > 1 && gameStats.roundNumber < 10 ?
+                                <Button onClick={newRound} style={{background:"#D29D2B"}}>
+                                    <Typography color="#557C55">
+                                        Next Round
+                                    </Typography>
+                                </Button>
+                                :
+                                <Button onClick={endGame} style={{background:"#D29D2B"}}>
+                                    <Typography color="#557C55">
+                                        See Results!
+                                    </Typography>
+                                </Button>
+                            }
+                        </Paper>
+                    </Box>
+                </Box>
+            </ThemeProvider>
         </div>)
     }
 
@@ -131,25 +199,33 @@ export function LeaderboardPage() {
     // Only goes down to h4, the top 3 get special header sizes
     function SizedHeaderFromIndex(idx: number, entry: any) {
         if (idx === 1) {
-            return (<h1><FaMedal color="gold" /> {idx}. {entry[0]} {entry[1]}</h1>)
+            return (<Box sx={{mb:1}}><Typography variant='h4' color="#AD8B73"><FaMedal color="gold" /> {idx}. {entry[0]} {entry[1]}</Typography></Box>)
         } else if (idx === 2) {
-            return (<h2><FaMedal color="silver" /> {idx}. {entry[0]} {entry[1]}</h2>)
+            return (<Box sx={{mb:1}}><Typography variant='h5' color="#AD8B73"><FaMedal color="silver" /> {idx}. {entry[0]} {entry[1]}</Typography></Box>)
         } else if (idx === 3) {
-            return (<h3><FaMedal color="bronze" /> {idx}. {entry[0]} {entry[1]}</h3>)
+            return (<Box sx={{mb:1}}><Typography variant='h6' color="#AD8B73"><FaMedal color="bronze" /> {idx}. {entry[0]} {entry[1]}</Typography></Box>)
         } else {
-            return (<h4>{idx + 1}. {entry[0]} {entry[1]}</h4>)
+            return (<Box sx={{mb:1}}><Typography variant='h6' color="#AD8B73">{idx + 1}. {entry[0]} {entry[1]}</Typography></Box>)
         }
     }
 
     function FinalResults() {
         return (
             <div>
-                <h4>Final Results</h4>
-                <FadeIn delay={500} transitionDuration={1000}>
-                    {Object.entries(playerScores).sort((a: any, b: any) => b[1] - a[1]).map((entry, idx) => (
-                        SizedHeaderFromIndex(idx + 1, entry)
-                    ))}
-                </FadeIn>
+                <ThemeProvider theme={islandTheme}>
+                    <Box bgcolor='#D29D2B' sx={{my:3}}>
+                        <Box sx={{mx:3, my:3, py:3}}>
+                            <Paper elevation={3} sx={{py: 2, px: 2}} style={{background: "#557C55"}}>
+                                <Paper elevation={3} sx={{py: 2, px: 2, mb:2, mx:6}} style={{background: "#85F4FF"}}><Typography variant='h4'color='#D29D2B'>Final Results</Typography></Paper>
+                                    <FadeIn delay={500} transitionDuration={1000}>                  
+                                            {Object.entries(playerScores).sort((a: any, b: any) =>b[1] - a[1]).map((entry, idx) => (
+                                               SizedHeaderFromIndex(idx + 1, entry)
+                                            ))}
+                                    </FadeIn>
+                            </Paper>
+                        </Box>
+                    </Box>
+                </ThemeProvider>
             </div>
         )
     }
